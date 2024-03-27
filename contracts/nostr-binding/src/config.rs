56f693aec00843914fbed44f58820be3742f5313
RetricSu <inneverland2013@gmail.com>
2024-03-27 14:05:06 +0800

    feat: add validate transfer test

--- contracts/nostr-binding/src/config.rs
+++ contracts/nostr-binding/src/config.rs
@@ -1,6 +1,6 @@
 pub const CELL_OUTPOINT_TAG_NAME: &str = "cell_outpoint";
 pub const CELL_TYPE_ID_TAG_NAME: &str = "cell_type_id";
 
-pub const ASSET_META_KIND: u16 = 23332;
+pub const _ASSET_META_KIND: u16 = 23332;
 pub const ASSET_MINT_KIND: u16 = 23333;
 pub const ASSET_CONSUME_KIND: u16 = 23334;
