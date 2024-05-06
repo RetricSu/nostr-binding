bca798bd5cfdec0bbe8112ad50978714d718abcc
RetricSu <inneverland2013@gmail.com>
2024-05-06 10:11:18 +0800

    refactor: use nostr-lock for demo app (#12)
    * refactor: use nostr-lock for demo app
    
    * fix: remove outside package-lockl

--- app/nostr-mint/app/conmponents/asset-button.tsx
+++ app/nostr-mint/app/conmponents/asset-button.tsx
@@ -1,19 +1,25 @@
-import { useContext } from "react";
+import { ReactNode, useContext } from "react";
 import { SingerContext } from "~/context/signer";
 import { Event } from "@rust-nostr/nostr-sdk";
 
 export interface AssetButtonProp {
   assetEvent: Event | undefined;
-  setResult: (res: string) => void;
+  setResult: (res: string | ReactNode) => void;
 }
 
 export function AssetButton({ setResult, assetEvent }: AssetButtonProp) {
   const context = useContext(SingerContext);
-  const nostrSigner = context.nostrSigner!;
 
   const onClick = async () => {
-	console.log(assetEvent?.asJson())
-    setResult(assetEvent?.asJson() || "not found");
+    const jsonEvent = assetEvent?.asJson();
+    if (jsonEvent) {
+      return setResult(
+        <code className="whitespace-pre">
+          {JSON.stringify(JSON.parse(jsonEvent), null, 2)}
+        </code>
+      );
+    }
+    setResult("not found");
   };
 
   return (
